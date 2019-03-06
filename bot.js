84000e9125f35858b7ce787b3c70de13aa41ec15
M3roof <43769947+M3roof@users.noreply.github.com>
2019-03-06 07:48:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,105 @@
+const ServerID = "516364082127831040"; //اي دي السيرفر
+const ChannelID = "541717128131379204";// اي دي الروم
 
+console.log(`
+ 
+                                                                                                            
+                                                                                                            
+  ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+ /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+                                                                                                            
+                                                                                                            
+		
+ ▒█▀▀█ █▀▀█ █▀▀█ █▀▀▄ █▀▀█ ▀▀█▀▀ 
+ ▒█▄▄█ █▄▄▀ █░░█ █▀▀▄ █░░█ ░░█░░ 
+ ▒█░░░ ▀░▀▀ ▀▀▀▀ ▀▀▀░ ▀▀▀▀ ░░▀░░ 
+         
+ ▒█▀▀█ █▀▀█ █▀▀ █▀▀▄ ░▀░ ▀▀█▀▀ █▀▀ 
+ ▒█░░░ █▄▄▀ █▀▀ █░░█ ▀█▀ ░░█░░ ▀▀█ 
+ ▒█▄▄█ ▀░▀▀ ▀▀▀ ▀▀▀░ ▀▀▀ ░░▀░░ ▀▀▀ 
+ 
+▒█▀▄▀█ ░▀░ █▀▀▄ █▀▀ █▀▀█ 
+▒█▒█▒█ ▀█▀ █░░█ █▀▀ █▄▄▀ 
+▒█░░▒█ ▀▀▀ ▀░░▀ ▀▀▀ ▀░▀▀ 
+        ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+        /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+`)
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+// ShaDoW_MaN
+console.log(`ShaDoW_MaN`)
+console.log(`
+______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+/_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╭╮╱╭━━━╮╱╱╱╱╱╱╭╮╭╮╱╱╱╱╭━╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╭╯╰╮┃╭━╮┃╱╱╱╱╱╱┃┣╯╰╮╱╱╱┃┃╰╯┃┃
+┃╰━╯┣━┳━━┫╰━┳━┻╮╭╯┃┃╱╰╋━┳━━┳━╯┣╮╭╋━━╮┃╭╮╭╮┣┳━╮╭━━┳━╮
+┃╭━━┫╭┫╭╮┃╭╮┃╭╮┃┃╱┃┃╱╭┫╭┫┃━┫╭╮┣┫┃┃━━┫┃┃┃┃┃┣┫╭╮┫┃━┫╭╯
+┃┃╱╱┃┃┃╰╯┃╰╯┃╰╯┃╰╮┃╰━╯┃┃┃┃━┫╰╯┃┃╰╋━━┃┃┃┃┃┃┃┃┃┃┃┃━┫┃
+╰╯╱╱╰╯╰━━┻━━┻━━┻━╯╰━━━┻╯╰━━┻━━┻┻━┻━━╯╰╯╰╯╰┻┻╯╰┻━━┻╯    
+╭━━━╮╭╮╱╱╱╱╱╭╮
+┃╭━╮┣╯╰╮╱╱╱╭╯╰╮
+┃╰━━╋╮╭╋━━┳┻╮╭╋┳━╮╭━━╮
+╰━━╮┃┃┃┃╭╮┃╭┫┃┣┫╭╮┫╭╮┃
+┃╰━╯┃┃╰┫╭╮┃┃┃╰┫┃┃┃┃╰╯┃╭┳┳╮
+╰━━━╯╰━┻╯╰┻╯╰━┻┻╯╰┻━╮┃╰┻┻╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+/_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+`)
+
+
+client.on('ready', () => console.log(`
+╭━━━╮╱╱╱╱╱╱╱╱╱╭━╮╭━╮╱╱╱╱╱╱╱╱╱╭━━━╮╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮╭╮
+╰╮╭╮┃╱╱╱╱╱╱╱╱╱┃┃╰╯┃┃╱╱╱╱╱╱╱╱╱┃╭━╮┣╯╰╮╱╱╱╭╯╰╮╱╱╱╱┃┃┃┃
+╱┃┃┃┣━━┳━╮╭━━╮┃╭╮╭╮┣┳━╮╭━━┳━╮┃╰━━╋╮╭╋━━┳┻╮╭╋━━┳━╯┃┃┃
+╱┃┃┃┃╭╮┃╭╮┫┃━┫┃┃┃┃┃┣┫╭╮┫┃━┫╭╯╰━━╮┃┃┃┃╭╮┃╭┫┃┃┃━┫╭╮┃╰╯
+╭╯╰╯┃╰╯┃┃┃┃┃━┫┃┃┃┃┃┃┃┃┃┃┃━┫┃╱┃╰━╯┃┃╰┫╭╮┃┃┃╰┫┃━┫╰╯┃╭╮╭╮
+╰━━━┻━━┻╯╰┻━━╯╰╯╰╯╰┻┻╯╰┻━━┻╯╱╰━━━╯╰━┻╯╰┻╯╰━┻━━┻━━╯╰╯╰╯
+`));// ShaDoW_MaN
+
+client.on('disconnect', () => console.log(`
+╭━╮╭━╮╱╱╱╱╱╱╱╱╱╭╮╱╭╮╱╱╱╱╭╮
+┃┃╰╯┃┃╱╱╱╱╱╱╱╱╱┃┃╱┃┃╱╱╱╱┃┃             
+┃╭╮╭╮┣┳━╮╭━━┳━╮┃╰━╯┣━━┳━╯┃
+┃┃┃┃┃┣┫╭╮┫┃━┫╭╯┃╭━╮┃╭╮┃╭╮┃
+┃┃┃┃┃┃┃┃┃┃┃━┫┃╱┃┃╱┃┃╭╮┃╰╯┃
+╰╯╰╯╰┻┻╯╰┻━━┻╯╱╰╯╱╰┻╯╰┻━━╯
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╭┳╮
+╰╮╭╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯╰╮╱╱╱╱┃┃┃
+╱┃┃┃┣┳━━┳━━┳━━┳━╮╭━╮╭━━┳━┻╮╭╋━━┳━╯┃┃
+╱┃┃┃┣┫━━┫╭━┫╭╮┃╭╮┫╭╮┫┃━┫╭━┫┃┃┃━┫╭╮┣╯
+╭╯╰╯┃┣━━┃╰━┫╰╯┃┃┃┃┃┃┃┃━┫╰━┫╰┫┃━┫╰╯┣╮
+╰━━━┻┻━━┻━━┻━━┻╯╰┻╯╰┻━━┻━━┻━┻━━┻━━┻╯
+`));
+
+client.on('reconnecting', () => console.log(`
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╭╮╱╭━━━╮╱╱╱╱╱╱╭╮╭╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╭╯╰╮┃╭━╮┃╱╱╱╱╱╱┃┣╯╰╮
+┃╰━╯┣━┳━━┫╰━┳━┻╮╭╯┃┃╱╰╋━┳━━┳━╯┣╮╭╋━━╮
+┃╭━━┫╭┫╭╮┃╭╮┃╭╮┃┃╱┃┃╱╭┫╭┫┃━┫╭╮┣┫┃┃━━┫
+┃┃╱╱┃┃┃╰╯┃╰╯┃╰╯┃╰╮┃╰━╯┃┃┃┃━┫╰╯┃┃╰╋━━┃
+╰╯╱╱╰╯╰━━┻━━┻━━┻━╯╰━━━┻╯╰━━┻━━┻┻━┻━━╯
+╭━╮╭━╮╱╱╱╱╱╱╱╱╱╭━━╮               
+┃┃╰╯┃┃╱╱╱╱╱╱╱╱╱╰┫┣╯
+┃╭╮╭╮┣┳━╮╭━━┳━╮╱┃┃╭━━╮
+┃┃┃┃┃┣┫╭╮┫┃━┫╭╯╱┃┃┃━━┫
+┃┃┃┃┃┃┃┃┃┃┃━┫┃╱╭┫┣╋━━┃
+╰╯╰╯╰┻┻╯╰┻━━┻╯╱╰━━┻━━╯           
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯╰╮
+┃╰━╯┣━━┳━━┳━━┳━╮╭━╮╭━━┳━┻╮╭╋┳━╮╭━━╮
+┃╭╮╭┫┃━┫╭━┫╭╮┃╭╮┫╭╮┫┃━┫╭━┫┃┣┫╭╮┫╭╮┃
+┃┃┃╰┫┃━┫╰━┫╰╯┃┃┃┃┃┃┃┃━┫╰━┫╰┫┃┃┃┃╰╯┣┳┳╮
+╰╯╰━┻━━┻━━┻━━┻╯╰┻╯╰┻━━┻━━┻━┻┻╯╰┻━╮┣┻┻╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+`));// ShaDoW_MaN
 
 
 
@@ -12,233 +107,40 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-var prefix = "#"
-    if (message.content.startsWith(prefix + 'clear')) {
-      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`ليس لديك برمشن[*MANAGE_MESSAGES*] `).catch(console.error);
-  message.delete()
-  if(!message.channel.guild) return;
-  let args = message.content.split(" ").slice(1);
-  
-  const messagecount = parseInt(args.join(' '));
-  
-  message.channel.fetchMessages({
-  
-  limit: messagecount
-  
-  }).then(messages => message.channel.bulkDelete(messages));
-  message.channel.sendMessage("", {embed: {
-    title: "``✏️✅ تــم مسح الشات ``",
-    color: 0x06DF00,
-    footer: {
-    
-    }
-    }}).then(msg => {msg.delete(3000)});
-  };
-  
-  });
+
+
+
+client.on('ready', async() => {
+    setInterval(()=>{
+    client.guilds.get(ServerID).channels.get(ChannelID).send('**Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , Drak , **')
+    },2000);
+})
 
 
 
 
 
 
-var prefix = "#"
-client.on('message', message => {
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
-  message.guild.member(user).ban(7, user);
-
-  const banembed = new Discord.RichEmbed()
-  .setAuthor(`BANNED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : banembed
-  })
-}
-});
-
-
-
-
-
-
-var prefix = "#"
-client.on('message', message => {
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "kick") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .kickable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
-  message.guild.member(user).kick();
-
-  const kickembed = new Discord.RichEmbed()
-  .setAuthor(`KICKED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : kickembed
-  })
-}
-});
-
-
-
-
-
-
-
-client.on("message", message => {
-    var prefix = "#";
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**يرجأء كتابة الرسالة المراد ارسالها للجميع**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                m.send(args);
-            });
-            const AziRo = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)   
-            .setTitle('✅| جاري ارسال رسالتك ') 
-            .addBlankField(true)
-            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-            .addField('📝| الرسالة ', args)
-            .setColor('RANDOM')  
-            message.channel.sendEmbed(AziRo);          
-        }
-        } else {
-            return;
-        }
-    });
-
-
-
-
-
-
-
-client.on('message', message => {
-
-    if(message.content === prefix + "#cl") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-           message.channel.overwritePermissions(message.guild.id, {
-         SEND_MESSAGES: false
-
-           }).then(() => {
-               message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
-           });
-             }
-//FIRE BOT
- if(message.content === prefix + "#op") {
-                     if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-           message.channel.overwritePermissions(message.guild.id, {
-         SEND_MESSAGES: true
-
-           }).then(() => {
-               message.reply("**__تم فتح الشات__:white_check_mark:**")
-           });
-             }
-             
+    client.on('message', message => {
+        var prefix = "3";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
       
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const developers = ["445406863048048642"]
-const adminprefix = "#";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
       
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wat')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Status You  ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
-      message.channel.send(`**Status You ${argresult} **`)
-}
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
 });
+
+
 
 
 
